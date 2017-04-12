f26bd2b1f4f200cbe95366f3a400163a4e3cceba
左岸清河 <174x226x@istc-a-007.epc.center.kobe-u.ac.jp>
2017-04-12 14:49:18 +0900

    task2

--- W02/vec3.js
+++ W02/vec3.js
@@ -31,4 +31,18 @@ Vec3.prototype.mid = function()
 {
     return Math.max(Math.min(this.x,this.y),Math.min(this.y,this.z),Math.min(this.x, this.z))
 }
+function heron (a,b,c)
+{
+    var s=(a+b+c)/2
+    return Math.sqrt(s*(s-a)*(s-b)*(s-c))
+}
 
+function dist(v1,v2)
+{
+    return Math.sqrt(Math.pow(v1.x-v2.x,2)+Math.pow(v1.y-v2.y,2)+Math.pow(v1.z-v2.z,2))
+}
+
+function AreaOfTriangle(v1,v2,v3)
+{
+    return heron(dist(v1,v2),dist(v2,v3),dist(v3,v1))
+}
