30bf796fac55b14367c1162f8e36f08ced99f032
左岸清河 <zuoanqh@live.com>
2017-05-09 14:04:52 +0900

    Task 1 with Phong & Lambertian

--- W08/main01.js
+++ W08/main01.js
@@ -22,7 +22,14 @@ function main()
     document.body.appendChild( renderer.domElement );
 
     var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
-    var material = new THREE.MeshLambertMaterial();
+	var material = new THREE.ShaderMaterial({
+		vertexColors: THREE.VertexColors,
+		vertexShader: document.getElementById('phong.vert').text,
+		fragmentShader: document.getElementById('gouraud.frag').text,
+		uniforms: {
+		light_position: { type: 'v3', value: light.position }
+		}
+	});
 
     var torus_knot = new THREE.Mesh( geometry, material );
     scene.add( torus_knot );
