e38e08df6716daed567709af62c8ba854d2460ce
左岸清河 <zuoanqh@live.com>
2017-06-04 22:37:02 +0900

    working example of a button

--- W15/w12_main_ex02.js
+++ W15/w12_main_ex02.js
@@ -1,28 +1,37 @@
+var surfaces;
+var volume;
+var screen;
 function main()
 {
-    var volume = new KVS.LobsterData();
-    var screen = new KVS.THREEScreen();
+    volume = new KVS.LobsterData();
+    screen = new KVS.THREEScreen();
 
-    screen.init( volume, {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        enableAutoResize: false
-    });
+	screen.init(volume, {width: window.innerWidth * 0.8, height: window.innerHeight,
+		targetDom: document.getElementById('display'), enableAutoResize: false});
 
     var bounds = Bounds( volume );
     screen.scene.add( bounds );
 
     var isovalue = 64;
-    var surfaces = Isosurfaces( volume, isovalue );
+    surfaces = Isosurfaces( volume, isovalue );
+	surfaces.name = "surfaces";
     screen.scene.add( surfaces );
 
     document.addEventListener( 'mousemove', function() {
         screen.light.position.copy( screen.camera.position );
     });
 
-    window.addEventListener( 'resize', function() {
-        screen.resize( [ window.innerWidth, window.innerHeight ] );
-    });
+	window.addEventListener('resize', function() {
+		screen.resize([ window.innerWidth * 0.8, window.innerHeight ]);
+	});
 
     screen.loop();
+}
+function btnISO_Click()
+{
+	var obj = screen.scene.getObjectByName("surfaces");
+	screen.scene.remove(obj);
+	surfaces =  Isosurfaces( volume,Math.round( document.getElementById("isovalue").value*255 ));
+	screen.scene.add(surfaces);
+	animate();
 }
